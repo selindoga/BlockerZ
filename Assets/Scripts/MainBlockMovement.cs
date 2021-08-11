46525a1dfc9002ec8e51968c50b6f2bc4792daf4
Selin Doga Orhan <73192627+selindoga@users.noreply.github.com>
2021-08-11 16:27:47 +0300

    tried to fix the bug
    failed but now the code is less bad

--- Assets/Scripts/MainBlockMovement.cs
+++ Assets/Scripts/MainBlockMovement.cs
@@ -12,21 +12,14 @@ public class MainBlockMovement : MonoBehaviour
     
     private float turnDegree = 90f;
     private bool turnsClockwise; // if slided left
+    private bool turnsCounterclockwise; // if slided right
     
-    private int clockwiseTurnNo = -2;
-    private int counterClockwiseTurnNo = 2;
+    private int clockwiseTurnNo = -1;
+    private int counterClockwiseTurnNo = 1;
     
-    private void Update()
+    private void Start()
     {
-        if (Input.touchCount > 0)
-        {
-            touch = Input.GetTouch(0);
-            
-            if (touch.phase == TouchPhase.Began) touchStartPosition = touch.position;
-            
-            if (touch.phase == TouchPhase.Ended) touchEndPosition = touch.position;
-        }
-        CalculateSlide();
+        StartCoroutine(DetectTouch());
     }
 
     private void CalculateSlide()
@@ -40,29 +33,51 @@ public class MainBlockMovement : MonoBehaviour
         }
         else if (deltaPosition < 0)
         {
-            turnsClockwise = false;
+            turnsCounterclockwise = true;
             StartCoroutine(Slide());
         }
-    }
 
+        turnsClockwise = false;
+        turnsCounterclockwise = false;
+        
+    }
+ 
     IEnumerator Slide()
     {
-        if (turnsClockwise)
+        if (turnsClockwise && !turnsCounterclockwise)
         {
            for (int i = 0; i <= turnDegree; i++) // these places are probably where the bug occurs
            {
                transform.Rotate(0f,0f,  clockwiseTurnNo);
-               yield return new WaitForSeconds(0.02222222f);
+               yield return new WaitForSeconds(0.02222222222f);
            } 
         } 
-        else if (turnsClockwise!)
+        else if (turnsCounterclockwise && !turnsClockwise)
         {
             for (int i = 0; i <= turnDegree; i++)
             {
                 transform.Rotate(0f,0f,  counterClockwiseTurnNo);
-                yield return new WaitForSeconds(0.02222222f);
+                yield return new WaitForSeconds(0.02222222222f);
             }
         }
-        
+    }
+
+    IEnumerator DetectTouch()
+    {
+        while (true)
+        {
+            if (Input.touchCount > 0)
+            {
+                touch = Input.GetTouch(0);
+            
+                if (touch.phase == TouchPhase.Began) touchStartPosition = touch.position;
+            
+                if (touch.phase == TouchPhase.Ended) touchEndPosition = touch.position;
+            
+                CalculateSlide();
+            }
+
+            yield return new WaitForSeconds(0.1f);
+        }
     }
 }
