ab730b48c8d35a295a2edcc51c95605750162916
Selin Doga Orhan <73192627+selindoga@users.noreply.github.com>
2021-09-07 23:00:03 +0300

    making manage the input

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputManager : MonoBehaviour
+{
+    private TouchControls touchControls;
+
+    private void Awake()
+    {
+        touchControls = new TouchControls();
+    }
+
+    private void OnEnable()
+    {
+        touchControls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        touchControls.Disable();
+    }
+
+    private void Start()
+    {
+        touchControls.Touch.TouchPress.started += ctx => StartTouch(ctx);
+        touchControls.Touch.TouchPress.canceled += ctx => EndTouch(ctx);
+    }
+
+    private void StartTouch(InputAction.CallbackContext context)
+    {
+        Debug.Log("Touch started " touchControls.Touch.TouchPosition.ReadValue<Vector2>());
+    }
+    private void EndTouch(InputAction.CallbackContext context)
+    {
+        Debug.Log("Touch ended");
+    }
+}
+
