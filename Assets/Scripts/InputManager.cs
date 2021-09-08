0f0401edd1c6bccc95972a2f7986ce65f647961c
Selin Doga Orhan <73192627+selindoga@users.noreply.github.com>
2021-09-08 15:18:28 +0300

    Update InputManager.cs
    trying

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -6,6 +6,16 @@ using UnityEngine.InputSystem;
 
 public class InputManager : MonoBehaviour
 {
+    public delegate void StartTouchEvent(Vector2 position, float time);
+
+    public event StartTouchEvent OnStartTouch;
+
+    public delegate void EndTouchEvent(Vector2 positiion, float time);
+
+    public event EndTouchEvent OnEndTouch;
+    
+    
+    
     private TouchControls touchControls;
 
     private void Awake()
@@ -31,11 +41,15 @@ public class InputManager : MonoBehaviour
 
     private void StartTouch(InputAction.CallbackContext context)
     {
-        Debug.Log("Touch started " touchControls.Touch.TouchPosition.ReadValue<Vector2>());
+        Debug.Log("Touch started " + touchControls.Touch.TouchPosition.ReadValue<Vector2>());
+        if (OnStartTouch != null)
+            OnStartTouch(touchControls.Touch.TouchPosition.ReadValue<Vector2>(), (float)context.startTime);
     }
     private void EndTouch(InputAction.CallbackContext context)
     {
         Debug.Log("Touch ended");
+        if (OnEndTouch != null)
+            OnEndTouch(touchControls.Touch.TouchPosition.ReadValue<Vector2>(), (float)context.time);
     }
 }
 
