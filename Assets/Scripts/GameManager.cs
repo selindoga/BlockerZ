1f0c0caf1b3f9f52f0f60b7c37ac528fd6900ff2
Selin Doga Orhan <73192627+selindoga@users.noreply.github.com>
2021-09-23 17:22:09 +0300

    get which scene the player currently in
    get the active scene

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,35 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // eğer scene b de isem 
+    private String sceneName;
+    
+    public static bool inSceneA;
+    public static bool inSceneB;
+    
+    private void Start()
     {
-        
-    }
+        Scene currentScene = SceneManager.GetActiveScene();
+        sceneName = currentScene.name;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (sceneName == "Scene A")
+        {
+            inSceneA = true;
+            inSceneB = false;
+        }
+        else if (sceneName == "Scene B")
+        {
+            inSceneB = true;
+            inSceneA = false;
+        }
     }
+    
+    
+    
+    
 }
