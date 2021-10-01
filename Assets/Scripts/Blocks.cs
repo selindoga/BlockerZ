eecc14fe4a09b54880b3d261e054a1fc0a0e13d7
Selin Doga Orhan <73192627+selindoga@users.noreply.github.com>
2021-10-01 19:09:20 +0300

    block movement: follows player's horizontal touch position [BUG]
    It does not follow player's horizontal touch input (does not give any error)
    Next time: remove MoveBlock_B() and rewrite that part for touch position in this script (Blocks.cs)

--- Assets/Scripts/Blocks.cs
+++ Assets/Scripts/Blocks.cs
@@ -4,15 +4,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Blocks : MonoBehaviour
-{
+{ 
+    // this code contains the movement of block prefabs: their falling, collision and interaction, their horizontal movement with the touch input
+    
+    private Camera CameraMain;
     private Rigidbody2D rb;
     private GameObject platform;
     private bool StartedFixedUpdate;
-    private SpawnArea _spawnArea;
+    private SpawnArea spawnArea;
+    private Touch touch;
     
     private void Awake()
     {
-        _spawnArea = GameObject.Find("Spawn Area").GetComponent<SpawnArea>(); // instead of _spawnArea = new SpawnArea(); because it cannot be written like this
+        CameraMain = Camera.main;
+        spawnArea = GameObject.Find("Spawn Area").GetComponent<SpawnArea>(); // instead of _spawnArea = new SpawnArea(); because it cannot be written like this
         rb = GetComponent<Rigidbody2D>();
     }
 
@@ -30,7 +35,7 @@ public class Blocks : MonoBehaviour
         
         StartedFixedUpdate = false;
         gameObject.transform.SetParent(platform.transform);
-        _spawnArea.StartedSpawn = true;
+        spawnArea.StartedSpawn = true;
     }
 
     private void OnEnable()
@@ -45,4 +50,28 @@ public class Blocks : MonoBehaviour
             rb.MovePosition(rb.position + new Vector2(0,-1) * Time.fixedDeltaTime);
         }
     }
+
+    private void Update()
+    {
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            touch = Input.GetTouch(0);
+            Debug.Log("inside the change block UPDATE");
+            // MoveBlock_B(touch.position);
+        }
+    }
+
+    // also write the code for design B that follows player's horizontal touch position
+    private void MoveBlock_B(Vector2 screenPosition) 
+        // have no idea how to combine this with the touch input 
+        // need to look a tutorial bout it
+    {
+        Vector3 screenCoortinates = new Vector3(screenPosition.x, screenPosition.y, CameraMain.nearClipPlane);
+        Vector3 worldCoordinates = CameraMain.ScreenToWorldPoint(screenCoortinates);
+        worldCoordinates.y = 0;
+        // worldCoordinates.z = 0;
+        transform.position = worldCoordinates;
+    }
+    
+    
 }
