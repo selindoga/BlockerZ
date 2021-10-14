d75a0ee819c6f6f5fa39c388356b2af847351bf2
Selin Doga Orhan <73192627+selindoga@users.noreply.github.com>
2021-10-14 20:41:47 +0300

    update comment

--- Assets/Scripts/external help/SwipeDetection.cs
+++ Assets/Scripts/external help/SwipeDetection.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using UnityEngine;
 
 // This code was copied from Samyam's video tutorial
-// The code was changed, edited and new codes were added by me
+// The code was changed, removed and new codes were added by me
 
 public class SwipeDetection : MonoBehaviour
 {
